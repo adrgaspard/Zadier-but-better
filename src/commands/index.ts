15e6910253847e8fb61f539b39167b19dc4876c9
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-02-15 01:49:21 +0100

    lint and format pass

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,7 +3,7 @@ import type { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { MIXTRAL } from './ai/mixtral';
 import { PING } from './ping';
 
-type SlashCommandDescriptor = SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">
+type SlashCommandDescriptor = SlashCommandBuilder | Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>;
 
 export type Command = {
     data: SlashCommandDescriptor;
